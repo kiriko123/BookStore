7550a021d3eceab49ef07238572a46fd9df72d3e
Tan Khang <tankhang101a@gmail.com>
2024-10-07 23:40:51 +0700

    delete MapStruct

--- be/src/main/java/com/bookstore/be/mapper/CategoryMapping.java
+++
@@ -1,8 +0,0 @@
-package com.bookstore.be.mapper;
-
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface CategoryMapping {
-
-}
--- be/src/main/java/com/bookstore/be/mapper/UserMapping.java
+++
@@ -1,7 +0,0 @@
-package com.bookstore.be.mapper;
-
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface UserMapping {
-}
